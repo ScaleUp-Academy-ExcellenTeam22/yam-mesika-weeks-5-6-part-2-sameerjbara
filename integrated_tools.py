9bf4fdffe088467ed895352c7e33ad500482b609
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-04-24 10:34:02 +0300

    Update integrated_tools.py

--- integrated_tools.py
+++ integrated_tools.py
@@ -1,11 +1,20 @@
 def interleave(*iterable):
-    """ merge iterable objects"""
-    merged_list=[]
-    for index in range(0,len(iterable)):
-        for item in iterable:
-            if(len(item)>index):
-             merged_list.append(item[index])
-    print(merged_list)
+    """:return interleave of the objects
+            Args:
+                *iterable (iterable): one iterable object or more .
+            Returns:
+                list: interleave of the objects.
+            Raises:
+                none.
+            Examples:
+                 interleave of 'abc' and [1,2,3] is ['a',1,'b',2,'c',3] .
+
+                >>> print(interleave('abc', [1, 2, 3],('!','@','#')))
+                ['a', '1', '!', 'b', '2', '@', 'c', '3', '#']
+
+            """
+    return [item[index] for index in range(0, len(iterable))
+            for item in iterable if len(item) > index]
 
 
-interleave('abc', [1, 2, 3], ('!', '@','#'))
+interleave('abc', [1, 2, 3], ('!', '@', '#'))
