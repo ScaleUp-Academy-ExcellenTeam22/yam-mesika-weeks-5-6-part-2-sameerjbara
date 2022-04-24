8d836f553e14d6bb5af4ad7addeb05e8d64fc756
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-04-24 10:45:10 +0300

    Update fast_and_reliable.py

--- fast_and_reliable.py
+++ fast_and_reliable.py
@@ -1,27 +1,44 @@
 import time
 
-"""intit the list and the set"""
-words_list = []
-words_set = set()
-file = open('words.txt')
-for word in file:
-    words_list.append(word)
-    words_set.add(word)
+
+def compare_runtime():
+    """Prints the average runtime for searching in different data structers
+                    Args: none
+                    Returns: none
+                    Raises: none
+                    Examples:
+                        average runtime in set is - 0.0
+                        average runtime in list is - 0.008217726230621338
+                        >>> compare_runtime()
+                        0.008217726230621338
+                        0.0
+                    """
+    file = open('words.txt')
+    words_list = [word for word in file]
+    words_set = set(words_list.copy())
+    print(average_runtime(words_list))
+    print(average_runtime(words_set))
+    file.close()
 
 
-def search(words):
-    """ this function search the word "zwitterion"
-     in the given data structure
-     and return the time that took the function to preform the task  """
-    start = time.time()
-
+def average_runtime(words):
+    """Returns the average runtime for searching 1000 time a word in a givin data structers
+                        Args:
+                            words (data struct) : the giving data structer
+                        Returns:
+                            int : the average run time for searching in the ds
+                        Raises: 
+                            none
+                        Examples:
+                            average runtime in set is - 0.0
+                            >>> average_runtime(("aa","bb","cc","dd"))
+                            0.0
+                        """
+    timer_start = time.time()
     for i in range(1, 1000):
         "zwitterion" in words
-
-    end = time.time()
-    return (end - start)
-
+    timer_end = time.time()
+    return (timer_end - timer_start) / 1000
 
 
-print(search(words_set))
-print(search(words_list))
+compare_runtime()
